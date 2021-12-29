abab95df0fed8f7c2a63e291c68d66f446685ed6
prajakta-devlekar <pdevlekar9909@gmail.com>
2021-12-29 14:19:43 +0530

    Update ReviewCard.js

--- src/components/pages/ReviewCard.js
+++ src/components/pages/ReviewCard.js
@@ -11,10 +11,10 @@ class ReviewCard extends Component {
                
             {data=>(
                 <div className='media mt-5 row'>
-                    <div className='col-1'>
-                    <img src={avatar} alt={id} style={{width:'50px'}} className='mr-3'/> 
+                    <div className='col-1 '>
+                    <img src={avatar} alt={id} style={{width:'50px'}} /> 
                     </div>
-                    <div className='ps-4 media-body col-11'>
+                    <div className='ps-5 media-body col-11'>
                         <h5 className='mt-0'>{name}</h5>
                         <p>{comment}</p>
                     </div>
